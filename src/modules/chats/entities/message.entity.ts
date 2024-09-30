f6536723838ba359e9ab0ab79a31e20357c675fd
Tit <tit.4mir@gmail.com>
2024-09-30 19:06:56 +0300

    feature: add column: chat_id. add validation.

--- src/modules/chats/entities/message.entity.ts
+++ src/modules/chats/entities/message.entity.ts
@@ -1,9 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Entity } from '@mikro-orm/core';
+import { Entity, Property } from '@mikro-orm/core';
 import { CreatedUpdatedEntity } from '../../../common/entities/created-updated.entity';
 
 @Entity({ tableName: 'messages' })
 export class MessageEntity extends CreatedUpdatedEntity {
     @ApiProperty()
+    @Property({ length: 1000 })
     readonly encryptMessage!: string;
+
+    @ApiProperty()
+    @Property()
+    chatId!: number;
 }
