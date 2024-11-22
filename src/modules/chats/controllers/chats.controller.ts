0ce1f52612e47be06ca5805f6d06f638f1764c75
Tit <tit.4mir@gmail.com>
2024-11-22 22:39:10 +0300

    fix: fixed bug in swagger

--- src/modules/chats/controllers/chats.controller.ts
+++ src/modules/chats/controllers/chats.controller.ts
@@ -1,18 +1,56 @@
-import { Body, Controller, Get, Headers, Param, Post, Query } from '@nestjs/common';
-import { ApiOkResponse } from '@nestjs/swagger';
+import { applyDecorators, Body, Controller, Get, Headers, Param, Post, Query, Type } from '@nestjs/common';
+import { ApiExtraModels, ApiOkResponse, getSchemaPath } from '@nestjs/swagger';
 import { ChatsService } from '../services/chats.service';
 import { CreateOpenChatDto } from '../dto/requests/create-open-chat.dto';
 import { QueryGetChatsDto } from '../dto/requests/query-get-chats.dto';
 import { ChatEntity } from '../entities/chat.entity';
 import { DataResponse } from '../../../common/swagger/data-response.dto';
+import { MessageEntity } from '../entities/message.entity';
+
+export function ApiData(type: Type, isArray = false) {
+    const array = {
+        type: 'array',
+        items: { $ref: getSchemaPath(type) },
+    };
+
+    const notArray = { $ref: getSchemaPath(type) };
+
+    return applyDecorators(
+        ApiExtraModels(ChatEntity, MessageEntity),
+        ApiOkResponse({
+            schema: {
+                anyOf: [
+                    {
+                        type: 'object',
+                        properties: {
+                            success: { type: 'boolean', example: true },
+                            data: isArray ? array : notArray,
+                        },
+                        required: ['success', 'data'],
+                    },
+
+                    {
+                        type: 'object',
+
+                        properties: {
+                            success: { type: 'boolean', example: false },
+                            data: {
+                                type: 'string',
+                            },
+                        },
+                        required: ['success', 'data'],
+                    },
+                ],
+            },
+        }),
+    );
+}
 
 @Controller('chats')
 export class ChatsController {
     constructor(private readonly chatsService: ChatsService) {}
 
-    @ApiOkResponse({
-        type: ChatEntity,
-    })
+    @ApiData(ChatEntity)
     @Post()
     createChat(
         @Body() body: CreateOpenChatDto,
@@ -21,15 +59,13 @@ export class ChatsController {
         return this.chatsService.createOpenChat(body.title, socketId);
     }
 
-    @ApiOkResponse({
-        type: ChatEntity,
-        isArray: true,
-    })
+    @ApiData(ChatEntity, true)
     @Get()
     async getChats(@Query() query: QueryGetChatsDto): Promise<DataResponse<ChatEntity[]>> {
         return await this.chatsService.getOpenChats(query.title, query.offset, query.limit);
     }
 
+    @ApiData(ChatEntity, true)
     @Get(':id')
     async getChat(@Param('id') id: number): Promise<DataResponse<string | ChatEntity>> {
         return this.chatsService.findChat(id);
--- src/modules/chats/controllers/messages.controller.ts
+++ src/modules/chats/controllers/messages.controller.ts
@@ -1,19 +1,17 @@
 import { Body, Controller, Get, Post, Query } from '@nestjs/common';
-import { ApiOkResponse } from '@nestjs/swagger';
 import { CreateMessageDto } from '../dto/requests/create-post-message.dto';
 import { MessagesService } from '../services/messages.service';
 import { MessageEntity } from '../entities/message.entity';
 import { QueryGetMessagesDto } from '../dto/requests/query-get-messages.dto';
 import { DataResponse } from '../../../common/swagger/data-response.dto';
 import { MessageTypeEnum } from '../types/message-type.enum';
+import { ApiData } from './chats.controller';
 
 @Controller('messages')
 export class MessagesController {
     constructor(private readonly messagesService: MessagesService) {}
 
-    @ApiOkResponse({
-        type: MessageEntity,
-    })
+    @ApiData(MessageEntity)
     @Post()
     createMessage(@Body() message: CreateMessageDto): Promise<DataResponse<MessageEntity | string>> {
         return this.messagesService.createMessage(
@@ -25,10 +23,7 @@ export class MessagesController {
         );
     }
 
-    @ApiOkResponse({
-        type: MessageEntity,
-        isArray: true,
-    })
+    @ApiData(MessageEntity, true)
     @Get()
     getMessages(@Query() query: QueryGetMessagesDto): Promise<DataResponse<MessageEntity[]>> {
         return this.messagesService.getMessages(query.chatId, query.limit, query.offset, query.search);
