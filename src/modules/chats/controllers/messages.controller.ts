5feca04dd7bf58653fba401464b7697fd11fcf7e
ramzini22 <ramil.zinnatullin.02@mail.ru>
2025-01-06 17:16:20 +0300

    refactor: improve method and change response

--- src/modules/chats/controllers/messages.controller.ts
+++ src/modules/chats/controllers/messages.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { CreateMessageDto } from '../dto/requests/create-post-message.dto';
 import { MessagesService } from '../services/messages.service';
 import { MessageEntity } from '../entities/message.entity';
@@ -7,12 +8,13 @@ import { DataResponse } from '../../../common/swagger/data-response.dto';
 import { MessageTypeEnum } from '../types/message-type.enum';
 import { ApiData } from '../../../common/swagger/api-data.decorator';
 
+@ApiTags('Messages')
 @Controller('messages')
 export class MessagesController {
     constructor(private readonly messagesService: MessagesService) {}
 
-    @ApiData(MessageEntity)
     @Post()
+    @ApiData(MessageEntity)
     createMessage(@Body() message: CreateMessageDto): Promise<DataResponse<MessageEntity | string>> {
         return this.messagesService.createMessage(
             message.chatId,
@@ -23,8 +25,8 @@ export class MessagesController {
         );
     }
 
-    @ApiData(MessageEntity, true)
     @Get()
+    @ApiData(MessageEntity, true)
     getMessages(@Query() query: QueryGetMessagesDto): Promise<DataResponse<MessageEntity[]>> {
         return this.messagesService.getMessages(query.chatId, query.limit, query.offset, query.search);
     }
