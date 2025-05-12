ef40dac76e0fb8de93bf7cd7592a29015344d07b
Tit <tit.4mir@gmail.com>
2025-05-12 16:53:41 +0300

    fix: removed function IsNotForbiddenTitle

--- src/modules/chats/dto/requests/create-open-chat.dto.ts
+++ src/modules/chats/dto/requests/create-open-chat.dto.ts
@@ -1,14 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, MinLength } from 'class-validator';
 import { Transform } from 'class-transformer';
-import { IsNotForbiddenTitle } from '../validator/forbidden-words.validator';
 
 export class CreateOpenChatDto {
     @ApiProperty({ description: 'Chat name' })
     @IsString()
     @IsNotEmpty()
     @MinLength(3)
-    @IsNotForbiddenTitle()
     @Transform(({ value }: { value: string }) => value.slice(0, 1).toUpperCase() + value.slice(1))
     readonly title!: string;
 }
