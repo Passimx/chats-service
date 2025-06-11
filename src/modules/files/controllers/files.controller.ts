a326bde43f5b665042dc46a44660bbe114d1e98a
Tit <tit.4mir@gmail.com>
2025-06-11 22:45:58 +0300

    feature: swagger

--- src/modules/files/controllers/files.controller.ts
+++ src/modules/files/controllers/files.controller.ts
@@ -2,12 +2,16 @@ import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common'
 import { File, FileInterceptor } from '@nest-lab/fastify-multer';
 import { FilesService } from '../services/files.service';
 import { FilesEntity } from '../entity/files.entity';
+import { ApiData } from '../../../common/swagger/api-data.decorator';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Files')
 @Controller('files')
 export class FilesController {
     constructor(private readonly filesService: FilesService) {}
 
     @Post('upload')
+    @ApiData(FilesEntity)
     @UseInterceptors(FileInterceptor('file'))
     async upload(@UploadedFile() file: File): Promise<FilesEntity> {
         return await this.filesService.uploadFile(file);
