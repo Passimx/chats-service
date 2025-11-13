830e55ca06db80eb4bdb4dd38828787f79c293fa
TitProger <150430652+TitProger@users.noreply.github.com>
2025-11-13 11:39:57 +0300

    Update system-message-language.enum.ts

--- src/modules/chats/types/system-message-language.enum.ts
+++ src/modules/chats/types/system-message-language.enum.ts
@@ -1,5 +1,5 @@
 export enum SystemMessageLanguageEnum {
     create_chat = 'chat_is_create',
     create_avatar = 'avatar_is_create',
-    create_dialogue = 'create_dialogue',
+    create_dialogue = 'dialogue_is_create',
 }
