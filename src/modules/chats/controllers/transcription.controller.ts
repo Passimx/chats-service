228e1572e8c83c60626a26960e468f94452040ce
Tit <tit.4mir@gmail.com>
2025-11-16 20:39:59 +0300

    feature: add controller getTranscriptionVoice

--- src/modules/chats/controllers/transcription.controller.ts
+++ src/modules/chats/controllers/transcription.controller.ts
@@ -1,11 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { EventPattern, Payload } from '@nestjs/microservices';
 import { TranscriptionService } from '../services/transcription.service';
 import { MessageDto } from '../../queue/dto/message.dto';
 import { TranscriptionResponseDto } from '../dto/response/transcription-response.dto';
 import { TopicsEnum } from '../../queue/types/topics.enum';
+import { DataResponse } from '../../../common/swagger/data-response.dto';
 
-@Controller()
+@Controller('transcription')
 export class TranscriptionController {
     constructor(private readonly transcriptionService: TranscriptionService) {}
 
@@ -13,5 +14,10 @@ export class TranscriptionController {
     addTranscriptionVoice(@Payload() body: MessageDto) {
         const response = body.data.data as TranscriptionResponseDto;
         this.transcriptionService.addTranscriptionVoice(response.fileId, response.transcription);
+    }
+
+    @Get(':id')
+    getTranscriptionVoice(@Param('id') id: string): Promise<DataResponse<{ transcription: string } | string>> {
+        return this.transcriptionService.getTranscriptionService(id);
     }
 }
--- src/modules/chats/services/transcription.service.ts
+++ src/modules/chats/services/transcription.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@mikro-orm/nestjs';
 import { EntityRepository } from '@mikro-orm/postgresql';
 import { FileEntity } from '../entities/file.entity';
+import { DataResponse } from '../../../common/swagger/data-response.dto';
 
 @Injectable()
 export class TranscriptionService {
@@ -20,5 +21,15 @@ export class TranscriptionService {
         if (!file.transcriptionVoice || file.transcriptionVoice.trim().length === 0) {
             await this.fileRepository.nativeUpdate({ key: fileId }, { transcriptionVoice: transcriptionVoice });
         }
+    }
+
+    async getTranscriptionService(id: string): Promise<DataResponse<{ transcription: string } | string>> {
+        const file = await this.fileRepository.findOne({ key: id });
+
+        if (!file || !file.transcriptionVoice) {
+            return new DataResponse('Voice message not found');
+        }
+
+        return new DataResponse({ transcription: file.transcriptionVoice });
     }
 }
