d869a0c4344255bc0b33555e7846a8d2f3084498
TitProger <150430652+TitProger@users.noreply.github.com>
2025-11-16 22:14:53 +0300

    Add DialoguesService and DialoguesController to ChatsModule

--- src/modules/chats/chats.module.ts
+++ src/modules/chats/chats.module.ts
@@ -2,18 +2,22 @@ import { Module } from '@nestjs/common';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { QueueModule } from '../queue/queue.module';
 import { ChatsService } from './services/chats.service';
+import { DialoguesService } from './services/dialogues.service';
 import { ChatEntity } from './entities/chat.entity';
 import { MessageEntity } from './entities/message.entity';
+import { ChatKeyEntity } from './entities/chat-key.entity';
 import { FileEntity } from './entities/file.entity';
 import { ChatsController } from './controllers/chats.controller';
 import { MessagesController } from './controllers/messages.controller';
+import { DialoguesController } from './controllers/dialogues.controller';
 import { TranscriptionController } from './controllers/transcription.controller';
 import { MessagesService } from './services/messages.service';
 import { TranscriptionService } from './services/transcription.service';
 
 @Module({
-    imports: [MikroOrmModule.forFeature([ChatEntity, MessageEntity, FileEntity]), QueueModule],
-    providers: [ChatsService, MessagesService, TranscriptionService],
-    controllers: [ChatsController, MessagesController, TranscriptionController],
+    imports: [MikroOrmModule.forFeature([ChatEntity, MessageEntity, FileEntity, ChatKeyEntity]), QueueModule],
+    providers: [ChatsService, MessagesService, TranscriptionService, DialoguesService],
+    controllers: [ChatsController, MessagesController, TranscriptionController, DialoguesController],
 })
 export class ChatsModule {}
+
