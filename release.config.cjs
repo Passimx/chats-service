5bef047232c8a0e6a92224c25f4dfd118ca3875e
ramzini22 <ramil.zinnatullin.02@mail.ru>
2025-11-29 02:06:07 +0300

    fix: correct response with keys

---
+++ release.config.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+    branches: ['main'],
+    tagFormat: '${version}',
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        ['@semantic-release/changelog', { changelogFile: 'CHANGELOG.md' }],
+        ['@semantic-release/npm', { npmPublish: false }],
+        [
+            '@semantic-release/git',
+            {
+                assets: ['package.json', 'package-lock.json'],
+                message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+            },
+        ],
+        ['@semantic-release/github', { assets: ['CHANGELOG.md'] }],
+    ],
+};
