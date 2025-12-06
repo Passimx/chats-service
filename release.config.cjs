bb1a92be3fe25b09d9a51e807f4e1615d82a245d
ramzini22 <ramil.zinnatullin.02@mail.ru>
2025-12-06 09:21:01 +0300

    refactor: change release rules

--- release.config.cjs
+++ release.config.cjs
@@ -2,7 +2,23 @@ module.exports = {
     branches: ['main'],
     tagFormat: '${version}',
     plugins: [
-        '@semantic-release/commit-analyzer',
+        [
+            '@semantic-release/commit-analyzer',
+            {
+                releaseRules: [
+                    { type: 'breaking', release: 'major' },
+                    { type: 'feat', release: 'minor' },
+                    { type: 'fix', release: 'patch' },
+                    { type: 'refactor', release: 'patch' },
+                    { type: 'security', release: 'patch' },
+                    { type: 'style', release: 'patch' },
+                    { type: 'chore', release: false },
+                    { type: 'ci', release: false },
+                    { type: 'docs', release: false },
+                    { type: 'test', release: false },
+                ],
+            },
+        ],
         '@semantic-release/release-notes-generator',
         ['@semantic-release/changelog', { changelogFile: 'CHANGELOG.md' }],
         ['@semantic-release/npm', { npmPublish: false }],
@@ -10,7 +26,7 @@ module.exports = {
             '@semantic-release/git',
             {
                 assets: ['package.json', 'package-lock.json'],
-                message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+                message: 'chore(release): ${nextRelease.version}\n\n${nextRelease.notes}',
             },
         ],
         ['@semantic-release/github', { assets: ['CHANGELOG.md'] }],
