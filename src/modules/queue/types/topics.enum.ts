b14242593efac7156c156712631bb18aabd1bef2
ramzini22 <ramil.zinnatullin.02@mail.ru>
2026-01-29 17:44:14 +0300

    breaking: add auth api

--- src/modules/queue/types/topics.enum.ts
+++ src/modules/queue/types/topics.enum.ts
@@ -1,14 +1,21 @@
 export enum TopicsEnum {
     EMIT_TO_CHAT = 'emit_to_chat',
     EMIT_TO_USER_ROOM = 'emit_to_user_room',
+
     JOIN_USER_TO_CHAT = 'join_user_to_chat',
     JOIN_CONNECTION_TO_CHAT = 'join_connection_to_chat',
-    LEAVE_CONNECTION_FROM_CHAT = 'leave_connection_to_chat',
+    JOIN_CONNECTION_TO_USER_ROOM = 'join_connection_to_user_room',
+
     LEAVE_USER_FROM_CHAT = 'leave_user_to_chat',
+    LEAVE_CONNECTION_FROM_CHAT = 'leave_connection_to_chat',
+    LEAVE_CONNECTION_FROM_USER_ROOM = 'leave_connection_from_user_room',
 
     // SYSTEM_CHATS = 'system_chats',
     // PUT_SYSTEM_CHATS = 'put_system_chats',
 
     AUDIO_TRANSCRIPTION_REQUEST = 'audio_transcription_request',
     AUDIO_TRANSCRIPTION_RESPONSE = 'audio_transcription_response',
+    ONLINE = 'online',
+    OFFLINE = 'offline',
+    LOGOUT = 'logout',
 }
